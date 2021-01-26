1b390efc0cd1ad1fa21f48cb999965219e7a695a
Dilien Oeuvrard <74972237+Dili3n@users.noreply.github.com>
2021-01-26 11:51:50 +0100

    Add files via upload
    Voici le final de mon projet DONJON Harry Potter.
    Après 6 mois de python, j'ai enfin réussi à faire un jeu qui respecte mes attentes.
    Sur cette dernière version : 
    
    - Correction de plusieurs bugs. 
    - Ajouts d'un Easter Egg
    - Ajout d'un mode de jeu ( Pierre Feuille Ciseaux)
    - Ajout de salle de pets (pour acheter des compagnons)

---
+++ DONJON HARRY POTTER FINAL/textes.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Sat Nov 21 17:44:15 2020
+
+@author: xarfy
+"""
+
+aide = ("""
+            Types : 
+                
+                Boutique:
+                    
+                    Tu peux acheter des potions de HEAL ! 20HP / 200 Gallions
+                    
+                    Un ballais magique qui pourra te servir pour SKIP des salles
+                    
+                Entrainement:
+                    
+                    Tu donnes 100 Gallions a un entraineur qui lui te donnera en
+                    échange soit 5 (pour Harry Potter) soit 15 (pour Hermione)
+                    d'endurance
+                
+                Lancer:
+                    
+                    Lancer la partie
+        
+        """)
+
+presentation1 = ("Bonjour jeune Aventurier et bienvenue dans le Donjon Harry Potter !")
+
+presentation2 = ("Pendant ton aventure tu seras confronté à combattre plusieurs monstres.")
+presentation2_1 = ("Gare à toi ! Si tu t'aventure dans ce chateau cela")
+presentation2_2 = ("sera à tes risques et périls. Tu devras réunir les 7 Horcrux afin de battre Voldemort !")
+presentation2_3 = ("Pour que le monde des sorciers redeviennent un monde paisible !")
+
+presentation3 = ("Pour te confronter aux monstres du monde des sorciers je t'invite à appuyer sur Entrer")
+
+hg1 = ("Bonjour jeune aventurier je suis Hermione Granger. Je suis ici pour t'entrainer.")
+hg1_2 = ("On va apprendre à faire voler une plume. Dit Wingardium Leviosa")
+
+hg2 = ("C'est Levioooooooooosa... et pas LeViOsA, Entraine-toi encore tu y arriveras un jour.")
+
+hp1 = ("Oui, c'est bien moi, Harry Potter... Aujourd'hui je vais t'initier au sortilège du stupéfix !")
+hp1_2 = ("Tout est dans le geste de la baguette. Tu as juste à crier stupéfix !")
+
+hp2 = ("Ah ce n'est pas nul mais pas parfait ! Tu y arriveras bientôt enfin, je l'espère")
